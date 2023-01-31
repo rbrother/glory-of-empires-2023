6ece49bfb488e757b1e89aad73fbdd211393a132
Robert Brotherus <robert@iki.fi>
2023-01-31 22:44:36 +0200

    Create ship-drag-targets only to tile hovered on (better performance)

--- src/glory_of_empires_2023/view/board.cljs
+++ src/glory_of_empires_2023/view/board.cljs
@@ -4,6 +4,7 @@
     [re-frame.core :refer [subscribe dispatch reg-event-db reg-event-fx
                            reg-sub inject-cofx]]
     [vimsical.re-frame.cofx.inject :as inject]
+    [medley.core :refer [assoc-some]]
     [glory-of-empires-2023.debug :as debug]
     [glory-of-empires-2023.logic.tiles :refer [tile-width tile-height tile-center]]
     [glory-of-empires-2023.logic.utils :refer [mul-vec add-vec sub-vec distance]]
@@ -48,38 +49,42 @@
      :d "M 110 4    L 4 188    L 110 372
          L 322 372  L 428 188  L 322 4    L 110 4"}]])
 
-(def ship-location-size 16)
+(def ship-location-size 20)
 
-(defn ship-locations [{id :id :as tile}]
-  [:div.absolute
-   (for [x (range 0 tile-width ship-location-size)
-         y (range 0 tile-height ship-location-size)
-         :let [loc-center [(+ x (* 0.5 ship-location-size))
-                           (+ y (* 0.5 ship-location-size))]
-               offset (sub-vec loc-center tile-center)]
-         :when (< (distance offset) 160)]
-     ^{:key [x y]}
-     [:div.ship-loc {:style {:left x, :top y}
-                     ;; preventDefault in :on-drag-enter and :on-drag-over
-                     ;; identifies the tile as drop target for the browser
-                     :on-drag-enter #(do (.preventDefault %)
-                                       (.log js/console "drag-enter" id loc-center))
-                     :on-drag-leave #(.log js/console "drag-leave" id loc-center)
-                     :on-drag-over #(.preventDefault %)
-                     :on-drop #(dispatch [::drop-on-tile tile offset])}])])
+(defn ship-drag-targets [{id :id :as tile}]
+  (let [dragging-ship @(subscribe [::ship/drag-unit])
+        current-drag-loc @(subscribe [::drag-target id])]
+    [:div.absolute {:style {:visibility (if dragging-ship :visible :hidden)}}
+     (for [x (range 0 tile-width ship-location-size)
+           y (range 0 tile-height ship-location-size)
+           :let [loc-center [(+ x (* 0.5 ship-location-size))
+                             (+ y (* 0.5 ship-location-size))]
+                 offset (sub-vec loc-center tile-center)]
+           :when (< (distance offset) 160)]
+       (let [this-current? (= loc-center current-drag-loc)]
+         ^{:key [x y]}
+         [:div.ship-drop-loc
+          {:style (assoc-some {:left x, :top y, :width (dec ship-location-size),
+                               :height (dec ship-location-size)}
+                    :background (when this-current? "yellow"))
+           ;; preventDefault in :on-drag-enter and :on-drag-over
+           ;; identifies the tile as drop target for the browser
+           :on-drag-enter #(do (.preventDefault %)
+                             (dispatch [::drag-enter id loc-center]))
+           :on-drag-over #(.preventDefault %)
+           :on-drop #(dispatch [::drop-on-tile tile offset])}]))]))
 
 (defn tile [{[x y] :screen-pos :keys [image id units] :as tile}]
   (let [id-str (str/upper-case (name id))
         selected? (= id @(subscribe [::subs/selected-tile]))
-        hover-on? (= id @(subscribe [::closest-tile-to-cursor]))
-        dragging-ship @(subscribe [::ship/drag-unit])]
+        hover-on? @(subscribe [::hover-on-tile? id])]
     [:div.absolute {:style {:left x, :top y}}
      (when selected? [:div.tile-menu-wrap [tile-menu id]])
      [:div.tile [:img.tile {:src (str image-dir "Tiles/" image)
                             :style (when selected? {:filter "brightness(1.5)"})}]]
      [:div.tile-id id-str]
-     (when dragging-ship [ship-locations tile])
      [ship/view units]
+     (when hover-on? [ship-drag-targets tile])
      (when hover-on? [:div.highlight tile-highlight])]))
 
 (defn view []
@@ -88,6 +93,10 @@
      [:h1 "Glory of Empires"]
      [:div.board {:id "board"
                   :on-mouse-move #(dispatch [::board-mouse-move (event-board-pos %)])
+                  ;; mouse-move event are not generated during drag operation,
+                  ;; but drag-over are generated in similar way. We need that to
+                  ;; keep our custom hover-detection updated
+                  :on-drag-over #(dispatch [::board-mouse-move (event-board-pos %)])
                   :on-click #(dispatch [::board-click])}
       (for [tile-data board-data]
         ^{:key (:id tile-data)} [tile tile-data])]]))
@@ -100,6 +109,15 @@
   :<- [::subs/board-amended]
   :<- [::board-mouse-pos]
   (fn [[tiles mouse-pos] _] (closest-tile-to tiles mouse-pos)))
+
+(reg-sub ::hover-on-tile? :<- [::closest-tile-to-cursor]
+  (fn [closest-tile [_ tile-id]]
+    (= closest-tile tile-id)))
+
+(reg-sub ::drag-target
+  (fn [{:keys [drag-target]} [_ tile-id]]
+    (when (= (:tile-id drag-target) tile-id)
+      (:loc-center drag-target))))
 
 ;; events
 
@@ -118,6 +136,11 @@
              closest
              nil))}))
 
+(reg-event-db ::drag-enter
+  (fn [db [_ tile-id loc-center]]
+    (assoc db :drag-target
+      {:tile-id tile-id, :loc-center loc-center})))
+
 (reg-event-db ::drop-on-tile [debug/log-event]
   (fn [{:keys [drag-unit] :as db}
        [_ {tile-id :id} drop-pos]]
@@ -125,7 +148,8 @@
       drag-unit (update-in [:units drag-unit]
                   #(assoc %
                      :location tile-id
-                     :offset drop-pos)))))
+                     :offset drop-pos))
+      true (dissoc :drag-target))))
 
 (reg-event-db ::choose-system [debug/log-event]
   (fn [db _]
