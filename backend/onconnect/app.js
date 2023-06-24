e3a91b1f388870ac5de98b248e70c8eb89146451
Robert Brotherus <robert@iki.fi>
2023-06-24 14:31:01 +0300

    Simplify JS lambdas

--- backend/onconnect/app.js
+++ backend/onconnect/app.js
@@ -1,9 +1,6 @@
-// Copyright 2018-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
+const AWS = require('aws-sdk')
 
-const AWS = require('aws-sdk');
-
-const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
+const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION })
 
 exports.handler = async event => {
   const putParams = {
@@ -11,13 +8,13 @@ exports.handler = async event => {
     Item: {
       connectionId: event.requestContext.connectionId
     }
-  };
-
-  try {
-    await ddb.put(putParams).promise();
-  } catch (err) {
-    return { statusCode: 500, body: 'Failed to connect: ' + JSON.stringify(err) };
   }
 
-  return { statusCode: 200, body: 'Connected.' };
-};
+  try {
+    await ddb.put(putParams).promise()
+  } catch (err) {
+    return { statusCode: 500, body: 'Failed to connect: ' + JSON.stringify(err) }
+  }
+
+  return { statusCode: 200, body: 'Connected.' }
+}
--- backend/sendmessage/app.js
+++ backend/sendmessage/app.js
@@ -1,54 +1,46 @@
-// Copyright 2018-2020Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
+const AWS = require('aws-sdk')
 
-const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION })
 
-const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
-
-const { TABLE_NAME } = process.env;
+const { TABLE_NAME } = process.env
 
 exports.handler = async event => {
   let connectionData;
-  
   try {
-    connectionData = await ddb.scan({ TableName: TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
-  } catch (e) {
-    return { statusCode: 500, body: e.stack };
-  }
+      connectionData = await ddb.scan({ TableName: TABLE_NAME, ProjectionExpression: 'connectionId' }).promise()
+      const senderConnectionId = event.requestContext.connectionId
 
-  const senderConnectionId = event.requestContext.connectionId
-  
-  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-    apiVersion: '2018-11-29',
-    endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
-  });
-  
-  const inputData = JSON.parse(event.body)
-  // Here we could modify the message going to clients, now we pass on the message from one client to all as-is
-  const postData = JSON.stringify(inputData)
-  
-  const postCalls = connectionData.Items.map(async ({ connectionId }) => {
-    try {
-      if (connectionId != senderConnectionId) {
-          console.log("Sending client message: ", connectionId, postData)
-          await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
-      }
-    } catch (e) {
-      if (e.statusCode === 410) {
-        console.log(`Found stale connection, deleting ${connectionId}`);
-        await ddb.delete({ TableName: TABLE_NAME, Key: { connectionId } }).promise();
-      } else {
-        console.log(e)
-        throw e
-      }
-    }
-  })
-  
-  try {
-    await Promise.all(postCalls);
+      const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+        apiVersion: '2018-11-29',
+        endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
+      })
+
+      const inputData = JSON.parse(event.body)
+      // Here we could modify the message going to clients, now we pass on the message from one client to all as-is
+      const postData = JSON.stringify(inputData)
+
+      const postCalls = connectionData.Items.map(async ({ connectionId }) => {
+        try {
+          if (connectionId != senderConnectionId) {
+              console.log("Sending client message: ", connectionId)
+              await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise()
+          }
+        } catch (e) {
+          if (e.statusCode === 410) {
+            console.log(`Found stale connection, deleting ${connectionId}`)
+            await ddb.delete({ TableName: TABLE_NAME, Key: { connectionId } }).promise()
+          } else {
+            console.log(e)
+            throw e
+          }
+        }
+      })
+
+      await Promise.all(postCalls)
+      console.log("All Messages sent")
+      return { statusCode: 200, body: 'Data sent.' }
   } catch (e) {
-    return { statusCode: 500, body: e.stack };
+    console.log(e)
+    return { statusCode: 500, body: e.stack }
   }
-  console.log("All Messages sent")
-  return { statusCode: 200, body: 'Data sent.' };
-};
+}
