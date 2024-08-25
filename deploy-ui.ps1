612b3aa8d4b1658465d563786b3f0674c6640461
Robert Brotherus <robert@iki.fi>
2024-08-25 22:54:56 +0300

    Make UI deployment directly to backend/staticfiles/files folde which is used for lamda-deployment

--- deploy-ui.ps1
+++ deploy-ui.ps1
@@ -1,12 +1,8 @@
-# Deploy UI to S3 bucket. Separate deployment for Websocker / DynamoDB backend in backend-folder
+# S3 deployment not compatible with cognito, so deploy as part of Lambda files and serve from lambda
 npm install
-Remove-Item -Path ".\resources\public\js\compiled" -Recurse
+Remove-Item -Path ".\backend\staticfiles\files\js\compiled" -Recurse
 npm run release
-# S3 deployment not compatible with cognito
-# aws s3 cp .\resources\public  s3://glory-of-empires/ --recursive
-Remove-Item -Recurse -Force .\backend\staticfiles\files\
-Copy-Item -Path ".\resources\public" -Destination ".\backend\staticfiles\files" -Recurse -Force
 cd .\backend
 # Deploys to 'staticfiles' lambda at URL https://3jcv5yd21l.execute-api.eu-north-1.amazonaws.com/prod/
-.\deploy-backend.ps1
+sam deploy
 cd ..
